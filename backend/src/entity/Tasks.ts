488363a03e2918983030d058f847213c7abbac40
vynny21 <vynnyprudencio@gmail.com>
2020-06-08 03:00:24 -0300

    Finalização da API CRUD de tarefas com TypeScript, Express, TypeORM e Postgres

---
+++ backend/src/entity/Tasks.ts
@@ -0,0 +1,29 @@
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn 
+} from 'typeorm'
+
+@Entity()
+export class Tasks {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  title: string
+
+  @Column()
+  description: string
+
+  @Column({ default: false })
+  finished: Boolean
+
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,23 +1,24 @@
-import {UserController} from "./controller/UserController";
+import { Router, Request, Response } from 'express'
+import { 
+    getTasks, 
+    getTask, 
+    saveTask, 
+    updateTask, 
+    finishedTask, 
+    removeTask 
+} from './controller/TasksController'
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-}];
+ const routes = Router()
+
+routes.get('/', async (req: Request, res: Response) => {
+    return res.json({ message: 'Hello World' })
+})
+
+routes.get('/tasks', getTasks)
+routes.get('/tasks/:id', getTask)
+routes.post('/tasks', saveTask)
+routes.put('/tasks/:id', updateTask)
+routes.delete('/tasks/:id', removeTask)
+routes.patch('/tasks/:id', finishedTask)
+
+export default routes
